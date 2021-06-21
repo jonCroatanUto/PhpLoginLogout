801f4953486d0b1d4faa5492778270e165c04299
JonCroatan <43881219+jonCroatanUto@users.noreply.github.com>
2021-06-21 14:19:18 +0200

    finish

--- functions.php
+++ functions.php
@@ -3,28 +3,56 @@
 // message SESSIONS
 
     function notAllow(){
-     
+        echo "<div class='alert alert-warning d-flex align-items-center' role='alert'>";
+        echo " <svg class='bi flex-shrink-0 me-2' width='24' height='24' role='img' aria-label='Warning:'><use xlink:href='#exclamation-triangle-fill'/></svg>";
+
         echo $_SESSION["notAllowed"];
-       
+
+        echo "</div>";
         unset ($_SESSION["notAllowed"]);
     }
     function nothtinMatch(){
+        echo "<div class='alert alert-danger d-flex align-items-center' role='alert'>";
+        echo " <svg class='bi flex-shrink-0 me-2' width='24' height='24' role='img' aria-label='Danger:'><use xlink:href='#exclamation-triangle-fill'/></svg>";
+
         echo $_SESSION["nothingMatch"];
+
+        echo "</div>";
+        unset ($_SESSION["email"]);
         unset ($_SESSION["nothingMatch"]);
     }
 
     function notEmailMatch(){
+        echo "<div class='alert alert-danger d-flex align-items-center' role='alert'>";
+        echo "<svg class='bi flex-shrink-0 me-2' width='24' height='24' role='img' aria-label='Danger:'><use xlink:href='#exclamation-triangle-fill'/></svg>";
+
         echo $_SESSION["notEmailMatch"];
+
+        echo "</div>";
+        
         unset ($_SESSION["notEmailMatch"]);
+        unset ($_SESSION["email"]);
     }
 
     function notPassMatch(){
+        echo "<div class='alert alert-danger d-flex align-items-center' role='alert'>";
+        echo " <svg class='bi flex-shrink-0 me-2' width='24' height='24' role='img' aria-label='Danger:'><use xlink:href='#exclamation-triangle-fill'/></svg>";
+
         echo $_SESSION["notPassMatch"];
+
+        echo "</div>";
         unset ($_SESSION["notPassMatch"]);
+        unset ($_SESSION["email"]);
+       
     }
 
     function allow(){
+        echo "<div class='alert alert-success d-flex align-items-center' role='alert'>";
+        echo " <svg class='bi flex-shrink-0 me-2' width='24' height='24' role='img' aria-label='Success:'><use xlink:href='#check-circle-fill'/></svg>";
+
         echo $_SESSION["allowed"];
+
+        echo "</div>";
         unset($_SESSION["allowed"]);
     }
 
@@ -45,8 +73,8 @@
         
     } 
     function notAllowed(){
-        if($_SESSION["email"]!="jon.garciaorad.asencor@gmail.com"){
-            header("Location:./index.php?$notLoged=true");
+        if(!isset($_SESSION["email"])){
+            header("Location:./index.php");
             $_SESSION["notAllowed"]="You can't access here without login";
         }
     }
@@ -61,5 +89,12 @@
         unset($_SESSION);
         setcookie(session_name(),"",time() - 3600);
         session_destroy();
-        header("Location:./index.php");
+        header("Location:./index.php?logout=true");
+    }
+    function logoutMessatge(){
+        echo "<div class='alert alert-success d-flex align-items-center' role='alert'>";
+        echo "You just logOUt I hope to see you soon";
+        echo "</div>";
+       
+     
     }
--- server_variables.php
+++ server_variables.php
@@ -1,15 +1,31 @@
-<?php
-session_start();
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.min.css">
+  <title>Document</title>
+</head>
+<body>
+  <?php
+    session_start();
 
-$_SESSION['browser']= $_SERVER['HTTP_USER_AGENT'];
-$_SESSION['ip']= $_SERVER['REMOTE_ADDR'];
-$_SESSION['time']= time();
+    $_SESSION['browser']= $_SERVER['HTTP_USER_AGENT'];
+    $_SESSION['ip']= $_SERVER['REMOTE_ADDR'];
+    $_SESSION['time']= time();
 
-echo '<pre>';
-print_r  ($_SESSION);
+    echo '<pre>';
+    print_r  ($_SESSION);
 
-function findIni(){
-  echo  php_ini_loaded_file();
-}
+    function findIni(){
+      echo  php_ini_loaded_file();
+    }
 
-findIni();
+    findIni();
+  ?>
+
+  <a href="./index.php" class="btn btn-outline-primary">back</a>
+</body>
+</html>
+
